cca5ebf32ec6137703e8844541cc02037e403bba
Grant Simmons <gsimm706@gmail.com>
2020-02-28 00:11:07 -0500

    I don't know what I changed here

--- eugenics/eugenics_viz.py
+++ eugenics/eugenics_viz.py
@@ -3,8 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 
-data = pd.read_csv('sterilization.csv')
-states = data['state'].values
+data = pd.read_csv('sterilization.csv', index_col='state')
+data = data.T
+print(data.T)
+#states = data['state'].values
 
 
 
@@ -19,9 +21,15 @@ hspace = 0.60
 ind = np.arange(30)
 width = 0.25
 
+years = ('pre-1943',)
+years += tuple('{:04d}'.format(i) for i in range(1943,1964))
+print(years)
+
 fig = plt.figure(figsize=(10,10))
 plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
 ax = fig.add_subplot(111)
+ax.plot(data.index[4:], data['WI'][4:])
 
-chart = ax.plot(, value, width, color='lightgray')
+
+plt.show()
 
