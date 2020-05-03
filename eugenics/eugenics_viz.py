24c0947014ab78ae46b0df4e0948da899ef6d18d
Grant Simmons <gsimm706@gmail.com>
2020-05-03 18:11:51 -0400

    Eugenics vizualization script

--- eugenics/eugenics_viz.py
+++ eugenics/eugenics_viz.py
@@ -1,80 +1,85 @@
-'''
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as dates
-
-data = pd.read_csv('sterilization.csv', index_col='state')
-print("Original data:\n", data)
-#states = data['state'].values
-
-
-
-
-left = 0.125
-bottom = 0.11
-right = 0.90
-top = 0.88
-wspace = 0.20
-hspace = 0.60
-
-ind = np.arange(30)
-width = 0.25
-
-years = ('pre-1943',)
-years += tuple('{:04d}'.format(i) for i in range(1943,1964))
-print(years)
-
-fig = plt.figure(figsize=(10,10))
-plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
-ax = fig.add_subplot(111)
-print('indexes:\n',data.index[4:])
-print('WI data:\n',data['WI'][4:])
-#ax.plot(data.index[4:], data['WI'][4:])
-#for state in data:
-#    print(data[state][0])
-#    ax.plot(data[state][4:])
-
-
-ind = np.arange(21)
-print(data['WI'][4:].values)
-print(ind)
-chart = ax.bar(ind, data['WI'][4:].values)
-print(data.index.values)
-plt.xticks(ind, data.index[4:].values)
-plt.legend(data)
-
-plt.show()
-'''
-
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
 import pandas as pd
-#%matplotlib inline
+from math import ceil
 
 data = pd.read_csv('sterilization.csv', index_col='state')
-print("Original data:\n", data)
-#states = data['state'].values
 
 usa = gpd.read_file('zip://cb_2018_us_state_20m.zip')
 #usa = usa.to_crs({'init' :'epsg:4326'})
-print(data.index)
 merged = pd.merge(usa, data, left_on=usa.STUSPS, right_index=True, how='left').fillna(0, downcast='infer')
 merged = merged[(merged.STUSPS != 'PR') & (merged.STUSPS != 'HI') & (merged.STUSPS != 'AK')]
-print(merged)
+
+left = 0.13
+bottom = 0.11
+right = 0.90
+top = 0.88
+wspace = 0.00
+hspace = 0.64
 
 num = 22
-per_row = 2
+per_row = 4
 
-fig, ax = plt.subplots(11, figsize=(10,10), ncols=per_row, sharex=True, sharey=True, squeeze=False)
-print(ax[0])
+fig, ax = plt.subplots(ceil(num/per_row), figsize=(10,10), ncols=per_row, sharex=True, sharey=True, squeeze=False)
+fig.suptitle('Relative Popularity of Compulsory Sterilization in the United States')
+plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
 
 for index, year in enumerate(merged.columns[13:]):
-    #ax[index] = fig.add_subplot(per_row, 4, index + 1)
-    #ax[index].axes.set_aspect('equal')
-    print(year)
-    merged.plot(ax=ax[int(index / per_row), index % per_row], column=year)
-plt.show()
-#usa[usa.STUSPS == 'FL'].plot(ax=ax, column='1955')
+    merged.plot(ax=ax[int(index / per_row), index % per_row], column=year, cmap='binary', edgecolor='black', alpha=.9) #legend=True
+    ax[int(index / per_row), index % per_row].set_xlabel(year)
 
+for index in range(ceil(num/per_row) * per_row):
+    xy = (int(index / per_row), index % per_row)
+    ax[xy].spines['top'].set_visible(False) #Less ink
+    ax[xy].spines['right'].set_visible(False)
+    ax[xy].spines['left'].set_visible(False)
+    ax[xy].spines['bottom'].set_visible(False)
+    ax[xy].tick_params(
+    axis='both',
+    which='both',
+    bottom=False,
+    top=False,
+    left=False,
+    right=False,
+    labelbottom=False)
+    ax[xy].set_yticklabels([])
+
+    
+plt.savefig(fname='choropleth.png', format='png', dpi=300)
+#plt.show()
+
+
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111)
+regions = { 'southeast' : {},
+            'southwest' : {},
+            'west' : {},
+            'northeast' : {},
+            'midwest' : {}   }
+lines = {}
+for key in regions:
+    region_data = data.loc[data['region'] == key]
+    for index, year in enumerate(region_data.columns[4:]):
+        for row_index, row in region_data.iterrows():
+            try:
+                regions[key][year] += row[year]
+            except:
+                regions[key][year] = row[year]
+    lines[key] = plt.plot(list(regions[key].keys()), list(regions[key].values()))
+    ax.annotate(key, xy=(0.98,(regions[key]['1963']/825)), xytext=(0.98,(regions[key]['1963']/825)), horizontalalignment='left', verticalalignment='center', textcoords='axes fraction', color=lines[key][0].get_color())
+ax.set_title('Compulsory Sterilization (By Region)')
+ax.set_ylabel('Number of\nPeople\nSterilized', rotation=0)
+ax.yaxis.set_label_coords(-0.1,0.5)
+ax.set_xlabel('Year')
+ax.spines['top'].set_visible(False) #Less ink
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.xaxis.grid(color='gainsboro', linestyle='dashed')
+ax.yaxis.grid(color='whitesmoke', linestyle='dashed')
+plt.ylim((-2,825))
+plt.xticks(list(regions[key].keys()), rotation=-90, va='center', ha='left', rotation_mode='anchor')
+
+plt.savefig(fname='sterilization.png', format='png', dpi=300)
+plt.show()
